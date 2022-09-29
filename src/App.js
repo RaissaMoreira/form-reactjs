0e22624c546e45b84b6e738e89ae699776e3c9d5
RaissaMoreira <raissamoreira80@gmail.com>
2022-09-29 14:46:08 -0300

    useContext validacoes cadastro

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import FormCadastro from './components/FormCadastro/FormCadastro';
 import { Container, Typography  } from '@mui/material';
 import { validarCPF, validarSenha } from "./models/cadastro";
+import ValidacoesCadastro from './contexts/ValidacoesCadastro';
 
 function App() {
   return (
     <Container component='article' maxWidth='sm'>
       <Typography variant='h3' component='h1' align='center'>Formulário de Cadastro</Typography >
-      <FormCadastro aoEnviar={ aoEnviarForm } validacoes={{ cpf: validarCPF, senha: validarSenha, nome:validarSenha }}/>
+      <ValidacoesCadastro.Provider value={{ cpf: validarCPF, senha: validarSenha, nome:validarSenha }}>
+        <FormCadastro aoEnviar={ aoEnviarForm }/>
+      </ValidacoesCadastro.Provider>
     </Container>
   );
 }
--- src/components/FormCadastro/FormCadastro.jsx
+++ src/components/FormCadastro/FormCadastro.jsx
@@ -5,7 +5,7 @@ import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
-function FormCadastro({ aoEnviar, validacoes }) {
+function FormCadastro({ aoEnviar }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDadosColetados] = useState({});
 
@@ -16,9 +16,9 @@ function FormCadastro({ aoEnviar, validacoes }) {
   })
 
   const formularios = [
-    <DadosUsuario aoEnviar={ coletarDados } validacoes={ validacoes } />,
-    <DadosPessoais aoEnviar={ coletarDados } validacoes={ validacoes }/>,
-    <DadosEntrega aoEnviar={ coletarDados } validacoes={ validacoes } />,
+    <DadosUsuario aoEnviar={ coletarDados } />,
+    <DadosPessoais aoEnviar={ coletarDados } />,
+    <DadosEntrega aoEnviar={ coletarDados } />,
     <Typography variant="h5">Obrigada pelo Cadastro!</Typography>
   ];
 
